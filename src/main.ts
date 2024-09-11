79631ccf7fecbe66109cfe14450b5dbe6877dda2
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-09-11 17:57:47 +0200

    feat: add dotenv configuration and sample file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -10,10 +12,11 @@ async function bootstrap() {
     .setDescription(
       'GengarAPI, an API to provide better formatted data from PokeAPI for FlutterDex App',
     )
+    .setVersion(process.env.VERSION)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
