1d5958f15412e9b8e2efd1688d2a9bbbb0e54c19
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-09-11 19:53:39 +0200

    feat: use the base url from pokeapi and custom error handling

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConnectionException } from './common/errors';
 
 @Controller('')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    return await this.appService.getHello();
   }
 
   @Get('/ping')
-  ping(): Record<string, string> {
-    return this.appService.ping();
+  async ping(): Promise<Record<string, string>> {
+    try {
+      const response = await this.appService.ping();
+      const responseKeys = Object.keys(response);
+
+      if (responseKeys.length === 0) {
+        throw new ConnectionException(
+          'No data available: unable to connect to the data source.',
+        );
+      }
+    } catch (error) {
+      throw new ConnectionException(
+        `Something went wrong while connecting to data source. Error message: ${error.message}`,
+      );
+    }
+
+    return { ping: 'pong' };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to GengarAPI!\nIf you are here it means that you like pokemon and programming, I also do!\nThis API is meant to act as a format layer for FlutterDex app with PokeAPI as a source of data (please check it out: https://pokeapi.co/)';
+  async getHello(): Promise<string> {
+    return await 'Welcome to GengarAPI!\nIf you are here it means that you like pokemon and programming, I also do!\nThis API is meant to act as a format layer for FlutterDex app with PokeAPI as a source of data (please check it out: https://pokeapi.co/)';
   }
 
-  ping(): Record<string, string> {
-    return { ping: 'pong' };
+  async ping(): Promise<Record<string, string>> {
+    try {
+      const response = await fetch(process.env.BASE_URL);
+
+      if (!response.ok) {
+        throw new ServiceUnavailableException();
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
